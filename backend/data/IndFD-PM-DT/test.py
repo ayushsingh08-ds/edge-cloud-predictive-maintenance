0d3885232df55dfc91f1a47a9c36361547ef0a43
sadgi <sadgi2204jaiswal@gmail.com>
2026-04-21 09:57:14 +0530

    hn

--- backend/data/IndFD-PM-DT/test.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-import pandas as pd
-
-base_dir = Path(__file__).resolve().parent
-csv_path = base_dir / "IndFD-PM-DT dataset.csv"
-
-df = pd.read_csv(csv_path)
-print(df["RUL"].min(), df["RUL"].max(), df["RUL"].mean())
